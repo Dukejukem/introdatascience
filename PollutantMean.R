7a2804adbb08fc7fcd6ec7cd1091332d3ec71761
Dukejukem <kapilchandran@yahoo.com>
2015-11-02 22:28:46 -0500

    Added 'complete' function

--- PollutantMean.R
+++ PollutantMean.R
@@ -1,5 +1,5 @@
-zero_adder <- function(x, k = 2) {
-  n <- k - floor(log10(x))
+zero_adder <- function(x, k = 3) {
+  n <- k - 1 - floor(log10(x))
   new_number <- paste(paste(rep(0, n), collapse = ''), x, sep = '')
   
   return(new_number)
@@ -25,4 +25,24 @@ pollutantmean <- function(directory, pollutant, id) {
 
 complete <- function(id) {
   
+  complete_matrix <- matrix(data = NA, nrow = length(id), ncol = 2)
+  entry <- 1
+  
+  for(i in id) {
+    n <- 0
+    table <- read.table(paste(zero_adder(i), ".csv", sep = ''), header = TRUE, sep = ",")
+    
+    for(j in 1:dim(table)[1]) {
+      if(!is.na(table[j, "sulfate"]) & !is.na(table[j, "nitrate"])) {
+        n <- n + 1
+      }
+      else {
+        next
+      }
+      complete_matrix[entry, 1] <- i
+      complete_matrix[entry, 2] <- n
+    }
+    entry <- entry + 1
+  }
+  return(complete_matrix)
 }
